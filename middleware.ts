0c7812990104ab1ef8b250375d7289673d724bf1
Collins Chirchir <collinskchirchir@gmail.com>
2024-05-16 11:37:33 +0300

    eslint prettier setup

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,15 @@
-import {auth, clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
-import {NextResponse} from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher([
-    '/'
-]);
+const isProtectedRoute = createRouteMatcher(["/"]);
 
 export default clerkMiddleware((auth, request) => {
-    if (isProtectedRoute(request)) {
-        auth().protect()
-    }
-    return NextResponse.next()
-
+  if (isProtectedRoute(request)) {
+    auth().protect();
+  }
+  return NextResponse.next();
 });
 
 export const config = {
-    matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
