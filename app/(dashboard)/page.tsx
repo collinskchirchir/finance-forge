b4668c16f578fb80aff075e72173057c89f24e06
Collins Chirchir <collinskchirchir@gmail.com>
2024-05-21 23:16:52 +0300

    fetch data through api endpoint for accounts

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>Dashboard Page</div>;
+
+  return (
+    <div>
+      Home Page
+    </div>
+  );
 }
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,22 +1,27 @@
-import { Hono } from "hono";
-import { handle } from "hono/vercel";
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
 
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import accounts from './accounts';
+import { HTTPException } from 'hono/http-exception';
 
-export const runtime = "edge";
+export const runtime = 'edge';
 
-const app = new Hono().basePath("/api");
+const app = new Hono().basePath('/api');
 
-app.get("/hello", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
-  if (!auth?.userId) {
-    return c.json({ error: "Unauthorized" });
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
   }
-  return c.json({
-    message: "Hello Next.js!",
-    userId: auth.userId,
-  });
+//   in case it's an unhandled error, standardize json output
+  return c.json({ error: 'Internal Error!' }, 500);
+
 });
+
+const routes = app
+  .route('/accounts', accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+
+export type AppType = typeof routes;
