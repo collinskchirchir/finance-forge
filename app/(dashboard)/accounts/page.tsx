c1a9da82ba9f207dfc8445ac0f4d4dc6200aa601
Collins Chirchir <collinskchirchir@gmail.com>
2024-05-24 10:03:45 +0300

    moved create account fn to accounts page

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,12 +1,24 @@
-import { Card } from '@/components/ui/card';
+'use client';
+import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
 import React from 'react';
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
 
 const AccountsPage = () => {
+  const { onOpen } = useNewAccount();
   return (
     <div>
-      <Card>
-
-        Accounts Page
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="line-clamp-1 text-xl">
+            Accounts Page
+          </CardTitle>
+          <Button size="sm" onClick={onOpen}>
+            <Plus className="mr-2 size-4" />
+            Add New
+          </Button>
+        </CardHeader>
       </Card>
     </div>
   );
