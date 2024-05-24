180db39ef44a4cd33fec65b82c0c082be5d9ec6e
Collins Chirchir <collinskchirchir@gmail.com>
2024-05-24 09:49:14 +0300

    added accounts page

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,15 @@
+import { Card } from '@/components/ui/card';
+import React from 'react';
+
+const AccountsPage = () => {
+  return (
+    <div>
+      <Card>
+
+        Accounts Page
+      </Card>
+    </div>
+  );
+};
+
+export default AccountsPage;
