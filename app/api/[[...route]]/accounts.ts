045dcc2c11692b769e8a9a719b09c191eae8b466
Collins Chirchir <collinskchirchir@gmail.com>
2024-05-25 12:14:14 +0300

    bulk-delete accounts api endpoint

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,9 +1,10 @@
+import { z } from 'zod';
 import { Hono } from 'hono';
 import { db } from '@/db/drizzle';
 import { createId } from '@paralleldrive/cuid2';
 import { accounts, insertAccountSchema } from '@/db/schema';
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
-import { eq } from 'drizzle-orm';
+import { and, eq, inArray } from 'drizzle-orm';
 import { zValidator } from '@hono/zod-validator';
 
 const app = new Hono()
@@ -23,22 +24,52 @@ const app = new Hono()
         .where(eq(accounts.userId, auth.userId));
       return c.json({ data });
     })
-  .post('/', clerkMiddleware(), zValidator('json', insertAccountSchema.pick({
-    name: true,
-  })), async (c) => {
-    const auth = getAuth(c);
-    const values = c.req.valid('json');
-    if (!auth?.userId) {
-      return c.json({ error: 'Unauthorized' }, 401);
-    }
+  .post(
+    '/',
+    clerkMiddleware(),
+    zValidator('json', insertAccountSchema.pick(
+      { name: true },
+    )),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid('json');
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorized' }, 401);
+      }
 
-    const [data] = await db.insert(accounts).values({
-      id: createId(),
-      userId: auth.userId,
-      ...values,
-    }).returning();
+      const [data] = await db.insert(accounts).values({
+        id: createId(),
+        userId: auth.userId,
+        ...values,
+      }).returning();
 
-    return c.json({ data });
-  });
+      return c.json({ data });
+    })
+  .post(
+    '/bulk-delete',
+    clerkMiddleware(),
+    zValidator('json', z.object({
+      ids: z.array(z.string()),
+    })),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid('json');
+
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorized' }, 401);
+      }
+      const data = await db
+        .delete(accounts)
+        .where(
+          and(
+            eq(accounts.userId, auth.userId),
+            inArray(accounts.id, values.ids),
+          ),
+        )
+        .returning({ id: accounts.id });
+      return c.json({ data });
+    },
+  )
+;
 
 export default app;
