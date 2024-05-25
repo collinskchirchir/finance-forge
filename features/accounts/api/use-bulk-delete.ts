5483230a3e888d75e4c1a1a565d5da7af212ec7c
Collins Chirchir <collinskchirchir@gmail.com>
2024-05-25 12:44:45 +0300

    bulk-delete accounts useQuery hook

---
+++ features/accounts/api/use-bulk-delete.ts
@@ -0,0 +1,28 @@
+import { toast } from 'sonner';
+import { InferRequestType, InferResponseType } from 'hono';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { client } from '@/lib/hono';
+
+// Type definitions for the response and request
+type ResponseType = InferResponseType<typeof client.api.accounts['bulk-delete']['$post']>
+type RequestType = InferRequestType<typeof client.api.accounts['bulk-delete']['$post']>['json']
+
+export const useBulkDeleteAccounts = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.accounts['bulk-delete'].$post({ json });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success('Accounts Deleted');
+      queryClient.invalidateQueries({ queryKey: ['accounts'] });
+      //   TODO: Also invalidate summary
+    },
+    onError: () => {
+      toast.error('Failed to Delete Accounts');
+    },
+  });
+  return mutation;
+};
