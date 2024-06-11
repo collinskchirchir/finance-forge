1526e52b86d88f925068ed84ddfd0048b29dd34e
Collins Chirchir <collinskchirchir@gmail.com>
2024-06-11 15:14:16 +0300

    feat: added date range filter on dashboard and transactions pages

--- components/filters.tsx
+++ components/filters.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AccountFilter from '@/components/account-filter';
+import DateFilter from '@/components/date-filter';
 
 const Filters = () => {
   return (
     <div className="flex flex-col items-center gap-y-2 lg:flex-row lg:gap-x-2 lg:gap-y-0">
       <AccountFilter />
+      <DateFilter />
     </div>
   );
 };
